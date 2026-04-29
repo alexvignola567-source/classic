eb6de812e1015923519433dcae05534b0d2d8032
alexvignola567-source <alexvignola567@gmail.com>
2026-04-29 23:41:50 +0200

    Add files via upload

---
+++ app/register/page.tsx
@@ -0,0 +1,15 @@
+
+        export default function RegisterPage() {
+          return (
+            <div style={{padding:'2rem'}}>
+              <h2>Register</h2>
+              <form>
+                <input placeholder="Name" />
+                <input placeholder="Email" />
+                <input placeholder="Password" type="password" />
+                <button>Create Account</button>
+              </form>
+            </div>
+          )
+        }
+    
